76e447cd48e1aa786a4eb1ce06e8087720bb5914
salunkheom <salunkheom97@gmail.com>
2025-06-01 10:20:46 +0530

    first trib

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Content from './CONTENT';
+
+function App() {
+  return (
+    <div className="App  bg-blue-500  h-screen  w-screen">
+      
+<Content />
+    </div> 
+  );
+}
+
+export default App;
---
+++ src/CONTENT.jsx
@@ -0,0 +1,25 @@
+
+
+ export default function Content() {
+    return(
+        <div className="content p-5 flex flex-col h-screen   text-white font-bold items-center text-justify ">
+
+      <h1 className="text-center rounded  px-5 mb-5 text-4xl">The Lord Krishna </h1>
+
+
+            <img src="https://i.pinimg.com/474x/f7/56/8f/f7568f2c02b5ec08582ddde20743e03b.jpg"
+            className="img" alt="" />
+            <p className="p-5">Krihna is a major deity in Hinduism, worshipped as the eighth avatar of Vishnu and also as the Supreme God. He is known for his compassion, protection, and love, and is a central figure in many Hindu scriptures like the Bhagavad Gita. Krishna's birthday, Krishna Janmashtami, is celebrated annually by Hindus. 
+Key Aspects of Krishna:
+Avatar of Vishnu: Krishna is considered the eighth avatar (incarnation) of the deity Vishnu. 
+Supreme God: He is also worshipped as the Supreme God in his own right. 
+Attributes: Krishna is revered for his compassion, protection, tenderness, and love. 
+Krishna Janmashtami: His birthday is celebrated as a major festival by Hindus. 
+Portrayal: Krishna is depicted in various ways in art and mythology, including as a child-god, a divine lover, and a hero. 
+In the Bhagavad Gita: He is a key character in the Bhagavad Gita, a sacred Hindu text. 
+Mother and Father: He was born to Vasudeva and Devaki. 
+Early Life: He was raised by Nanda and Yashoda in Gokula after being smuggled across the Yamuna River to escape his maternal uncle Kamsa. 
+Death: Krishna's death is said to have occurred 36 years after the Kurukshetra War</p>
+        </div>
+    )
+}
