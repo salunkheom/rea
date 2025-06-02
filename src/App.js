61a2b26fe254aeecfc0e871c0a8e2b9c5457e4a8
salunkheom <salunkheom97@gmail.com>
2025-06-02 10:56:02 +0530

    first react

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Content from './CONTENT';
 
 function App() {
   return (
-    <div className="App  bg-blue-500  h-screen  w-screen">
+    <div className="App  bg-blue-500 text-white  h-screen  w-screen">
       
 <Content />
     </div> 
